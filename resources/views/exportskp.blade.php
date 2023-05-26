e95a13fe14fa95ad4af439cfe13d4f5ff2190918
ridhowise <ridhowise1@gmail.com>
2023-05-26 08:57:24 +0800

    Test

--- resources/views/exportskp.blade.php
+++ resources/views/exportskp.blade.php
@@ -506,7 +506,7 @@ html *
             <td style="font-size:11px; border: 1px solid black;padding:1px">{{$items->indikator}}</td> 
             <td style="font-size:11px; border: 1px solid black;padding:1px">{{$items->target}}</td> 
             <td style="font-size:11px; border: 1px solid black;padding:1px">{{$items->realisasi}}</td> 
-            <td style="font-size:11px; border: 1px solid black;padding:1px"><img src="https://i.ibb.co/1GWnwCw/Screenshot-28.png" alt="">{{$items->umpan}}</td> 
+            <td style="font-size:11px; border: 1px solid black;padding:1px"><img style="width:10px"src="https://i.ibb.co/1GWnwCw/Screenshot-28.png" alt="">{{$items->umpan}}</td> 
 
             </tr>
 
